a64cb52def9988b66a58621e9ea451cda1fefc41
Ariestya Dibyanugraha <ariestya.dibyanugraha@gmail.com>
2019-02-05 10:53:04 +0700

    Change to Sliver instead of ListView.builder

--- lib/views/home/PlanetHomeBody.dart
+++ lib/views/home/PlanetHomeBody.dart
@@ -6,10 +6,23 @@ class PlanetHomeBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Expanded(
-        child: new ListView.builder(
-      itemBuilder: (context, index) => new PlanetRow(planets[index]),
-      itemCount: planets.length,
-      padding: new EdgeInsets.symmetric(vertical: 16),
+        child: new Container(
+      color: Colors.blue,
+      child: new CustomScrollView(
+        scrollDirection: Axis.vertical,
+        slivers: <Widget>[
+          new SliverPadding(
+            padding: const EdgeInsets.symmetric(vertical: 24),
+            sliver: new SliverFixedExtentList(
+              itemExtent: 152,
+              delegate: new SliverChildBuilderDelegate(
+                (context, index) => new PlanetRow(planets[index]),
+                childCount: planets.length,
+              ),
+            ),
+          )
+        ],
+      ),
     ));
   }
 }
