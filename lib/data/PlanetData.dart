5bf0c18ca44e2205ca74ec5a50fe18995c562894
Ariestya Dibyanugraha <ariestya.dibyanugraha@gmail.com>
2019-02-06 17:15:36 +0700

    Update planet data

--- lib/data/PlanetData.dart
+++ lib/data/PlanetData.dart
@@ -9,6 +9,7 @@ List<Planet> planets = [
     gravity: "Me-neither m/s",
     description: "The closest planet to the Sun",
     image: "assets/images/mercury.png",
+    picture: "https://c1.staticflickr.com/9/8105/8497927473_2845ae671e_b.jpg"
   ),
   const Planet(
     id: "2",
@@ -18,6 +19,7 @@ List<Planet> planets = [
     gravity: "3.711 m/s",
     description: "This is where the martian lives",
     image: "assets/images/mars.png",
+    picture: "https://www.nasa.gov/sites/default/files/thumbnails/image/pia21723-16.jpg"
   ),
   const Planet(
     id: "3",
@@ -27,6 +29,7 @@ List<Planet> planets = [
     gravity: "9.8 m/s",
     description: "This is where we lives",
     image: "assets/images/earth.png",
+    picture: "https://www.nasa.gov/sites/default/files/styles/full_width_feature/public/thumbnails/image/iss042e340851_1.jpg"
   ),
   const Planet(
     id: "4",
@@ -36,6 +39,7 @@ List<Planet> planets = [
     gravity: "dunno, 3,- m/s",
     description: "Weeeee",
     image: "assets/images/moon.png",
+    picture: "https://farm5.staticflickr.com/4086/5052125139_43c31b7012.jpg"
   ),
   const Planet(
     id: "5",
@@ -45,5 +49,6 @@ List<Planet> planets = [
     gravity: "flying man m/s",
     description: "Ho ho ho",
     image: "assets/images/neptune.png",
+    picture: "https://www.nasa.gov/sites/default/files/styles/full_width_feature/public/images/110411main_Voyager2_280_yshires.jpg"
   ),
 ];
